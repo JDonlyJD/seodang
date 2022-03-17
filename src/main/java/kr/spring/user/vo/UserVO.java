006ee1cc4d27548394822a9897d252a3f210dc74
soyeon_lim <lsy96060@naver.com>
2022-03-17 19:58:09 +0900

    커밋알람테스

---
+++ src/main/java/kr/spring/user/vo/UserVO.java
@@ -0,0 +1,11 @@
+package kr.spring.user.vo;
+
+public class UserVO {
+	private int user_num;
+	private String id;
+	private int auth;
+	private String name;
+	private String passwd;
+	private String phone;
+
+}
